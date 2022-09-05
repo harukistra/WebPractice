70bd66ae31b5bfb13a8ae14b550d636e958f8885
schoolharuki <sonefrree58@gmail.com>
2022-09-05 16:55:43 +0900

    consoleのerror除去

--- original/src/App.vue
+++ original/src/App.vue
@@ -1,57 +1,101 @@
 <template>
   <div v-if="logInOut === false">
-    <button
-      @click="SignIn"
-    >
-      Googleアカウントでログイン
-  </button>
+    <button @click="SignIn">Googleアカウントでログイン</button>
+
+    <div @click="SignIn2">click</div>
+    <div id="login-container"></div>
   </div>
 
   <div v-else>
     <button @click="SignOut">ログアウト</button>
 
+    <div>ようこそ</div>
+    <div>{{ showUser.name }}さん、こんにちは</div>
   </div>
-
 </template>
-<script>
 
-import { getAuth, signOut, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+<script>
+import {
+  getAuth,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth"
+import { db } from "./firebase"
+import { setDoc, doc, getDoc, addDoc, collection } from "firebase/firestore"
+
 export default {
   data() {
     return {
-      logInOut: false
+      logInOut: false,
+      userData: [],
+      showUser: [],
     }
   },
   methods: {
-    SignIn:async function() {
+    SignIn: async function () {
+      // クリックしたらgoogleアカウントでサインイン。userのデータをsetUserでfirebaseに登録。
       const provider = new GoogleAuthProvider()
 
       const auth = getAuth()
       await signInWithPopup(auth, provider)
         .then((result) => {
-          const credential = GoogleAuthProvider.credentialFromResult(result)
-          const token = credential.accessToken
+          // const credential = GoogleAuthProvider.credentialFromResult(result)
+          // const token = credential.accessToken
           const user = result.user
           // this.$router.push({ path: '/' })
-          console.log(credential)
-          console.log(token)
+          // console.log(credential)
+          // console.log(token)
+          // console.log(user)
+
+          this.setUser(user)
+          this.getUserData(user)
           console.log(user)
-        }).catch((error) => {
-          console.error(error)
-
         })
-        this.logInOut = !this.logInOut
-    }, 
-    SignOut:async function() {
-      const auth = getAuth() 
-      await signOut(auth).then(() => {
-        alert('signout')
-      }).catch((error) => {
-        console.log(error)
-      })
+        .catch((error) => {
+          console.error(error)
+        })
       this.logInOut = !this.logInOut
+    },
 
-    }
-  }
+    // SignIn2: async function () {
+    //   const ui = new firebaseui.auth.AuthUI(firebase.auth())
+    //   ui.start("#login-container", {
+    //     signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+    //   })
+    // },
+
+    SignOut: async function () {
+      const auth = getAuth()
+      await signOut(auth)
+        .then(() => {
+          alert("signout")
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+      this.logInOut = !this.logInOut
+    },
+
+    setUser(user) {
+      setDoc(doc(db, "users", user.displayName), {
+        userName: user.displayName,
+        ID: user.uid,
+      })
+    },
+
+    getUserData: async function (user) {
+      const docRef = doc(db, "users", user.displayName)
+      const docSnap = await getDoc(docRef)
+      this.userData.push({
+        name: docSnap.data().userName,
+        id: docSnap.data().ID,
+      })
+
+      // ロード時はuserDataが空なのでエラーになる。
+      this.showUser.name = this.userData[0].name
+      console.log(this.showUser)
+    },
+  },
 }
 </script>
